2e6a35a725e27406f4fa2c65105150618c4018c0
itimor <itimor@126.com>
2020-04-26 19:27:59 +0800

    add backtotop

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
     <router-view />
+    <el-backtop :visibility-height="100">
+      <el-tooltip placement="top" content="上车吧！骚年">
+        <svg-icon icon-class="rocket" style="width: 40px; height: 40px;" />
+      </el-tooltip>
+    </el-backtop>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
