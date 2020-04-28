75013fc776badaae2b1c31b66fac53c8b341397a
itimor <itimor@126.com>
2020-04-28 14:44:55 +0800

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -39,7 +39,8 @@ export const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index'),
-    hidden: true
+    hidden: true,
+    meta: { title: 'login' }
   },
   {
     path: '/404',
